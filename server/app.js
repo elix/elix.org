34ac6ee52795d1ec0e7d14b0e0009cf898445814
Jan Miksovsky <jan@quickui.org>
2017-04-06 17:18:46 -0700

    Clean up

--- server/app.js
+++ server/app.js
@@ -13,9 +13,7 @@ const renderReactRoute = require('./renderReactRoute');
 // Tell Express to serve up static content.
 app.use('/static', express.static(path.join(__dirname, '../public')));
 app.use('/elix', express.static(path.join(__dirname, '../node_modules/elix')));
-// app.use('/json', express.static(path.join(__dirname, '../build/docs')));
-// app.use('/markdown', express.static(path.join(__dirname, '../markdown')));
-
+ 
 //
 // Redirect http to https under Heroku
 //
