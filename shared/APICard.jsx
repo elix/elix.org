11ec9069f6faaeb1b19097aed7ddcda1cab64a27
Rob Bearman <robbear@hyperfine.com>
2017-04-07 15:39:25 +0000

    Add ParameterTable.jsx for better APICard.jsx code factoring

--- shared/APICard.jsx
+++ shared/APICard.jsx
@@ -1,5 +1,6 @@
 import { Component, h } from 'preact'; // jshint ignore:line
 import Markdown from './Markdown';
+import ParameterTable from './ParameterTable';
 
 
 /**
@@ -11,64 +12,25 @@ export default class APICard extends Component {
     const api = props.api;
     const params = api.params;
     
-    // Hold the parameter table rows
-    let rowArray = [];
-    
-    //
-    // Set to false if any of the parameters lacks a type field.
-    // This results in a table with two rather than three columns.
-    //
-    let useTypeColumn = true;
-    
-    // Set to true if we display the parameter table for this APICard
-    let useParameterTable = false;
-
     // Initialize the apiName variable
     let apiName = api.name;
     
     //
     // If this api is a function/member, we build a parameter list
-    // for display with the api (eg: foo(param1, param2) ), and
-    // we build a parameter table. If there are no parameters, we
-    // do neither.
+    // for display with the api (eg: foo(param1, param2) ).
     //
     let parameterList = '';
-    if (api.kind === 'function' && params.length > 0) {
-      for (let i = 0; i < params.length; i++) {
-        let param = params[i];
-        
-        // Build the parameter list for use in the api name display. The
-        // funky conditionalized code handles comma placements in the string.
-        parameterList = `${parameterList}${param.name}${(i+1) < params.length ? ', ' : ''}`;
-
-        //
-        // Build the function/member parameter table
-        //
-        let row;        
-        if (param.type && useTypeColumn) {
-          row = (
-            <tr>
-              <td>{param.name}</td>
-              <td><code>{param.type.names[0]}</code></td>
-              <td>{param.description}</td>
-            </tr>
-          );
-        }
-        else {
-          useTypeColumn = false;
+    if (api.kind === 'function') {
+      if (params.length > 0) {
+        for (let i = 0; i < params.length; i++) {
+          let param = params[i];
           
-          row = (
-            <tr>
-              <td>{param.name}</td>
-              <td>{param.description}</td>
-            </tr>
-          );
+          // Build the parameter list for use in the api name display. The
+          // funky conditionalized code handles comma placements in the string.
+          parameterList = `${parameterList}${param.name}${(i+1) < params.length ? ', ' : ''}`;
         }
-
-        rowArray.push(row);
       }
       
-      useParameterTable = true;
       apiName = `${apiName}(${parameterList})`;
     }
     
@@ -89,39 +51,6 @@ export default class APICard extends Component {
       '';
     
     //
-    // Finish up the table with its parts
-    //
-    const tHeader = (
-      <thead>
-        <tr>
-          <th>Param</th>
-          <th>Type</th>
-          <th>Description</th>
-        </tr>
-      </thead>
-    );
-    const tHeaderWithoutType = (
-      <thead>
-        <tr>
-          <th>Param</th>
-          <th>Description</th>
-        </tr>
-      </thead>
-    );
-    let tableBodyJSX = (
-      <tbody>
-        {rowArray}
-      </tbody>
-    );
-    let tableHeaderJSX = useTypeColumn ? tHeader : tHeaderWithoutType;
-    let tableJSX = useParameterTable ? (
-      <table>
-        {tableHeaderJSX}
-        {tableBodyJSX}
-      </table>
-    ) : '';
-
-    //
     // The final API card JSX
     //
     return (
@@ -129,7 +58,7 @@ export default class APICard extends Component {
         <h3>{apiName}</h3>
         <Markdown class="apiDescription" markdown={api.description}/>
         {definedByJSX}
-        {tableJSX}
+        <ParameterTable parameters={params}/>
       </div>
     );
   }
