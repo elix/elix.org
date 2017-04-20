f9a73095137c60ea199b4d58ee620bd2c9f45087
Rob Bearman <robbear@hyperfine.com>
2017-04-20 00:29:40 +0000

    Remove sidebar from Version page

--- shared/VersionPage.jsx
+++ shared/VersionPage.jsx
@@ -10,22 +10,10 @@ export default class VersionPage extends Component {
 
   render(props) {
 
-    const sideNavigation = (
-      <nav>
-        <ul>
-          <li><a href="#components">Components</a></li>
-          <li><a href="#overview">Overview</a></li>
-          <li><a href="#core-principles">Core Principles</a></li>
-          {/*<li><a href="#getting-started">Getting Started</a></li>*/}
-        </ul>
-      </nav>
-    );
-
     return (
       <PageTemplate
           headerTexture="/static/images/homeTexture.png"
           request={props.request}
-          sideNavigation={sideNavigation}
         >
         
         <section class="homeSection0">
