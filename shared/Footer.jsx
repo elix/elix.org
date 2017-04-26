80bc1bfff47b755e5d3efce8e5d00a7b0b87adaf
Jan Miksovsky <jan@quickui.org>
2017-04-26 16:53:37 -0700

    Tweak copyright notice text and styling.

--- shared/Footer.jsx
+++ shared/Footer.jsx
@@ -7,17 +7,17 @@ import { h } from 'preact'; // jshint ignore:line
 export default (props) => (
   <footer>
     <p>
-      © 2016-2017 Component Kitchen, Inc. and contributors to the Elix Project.
-      All Rights Reserved. ELIX and the Elix Project logo are registered or 
-      unregistered trademarks of Component Kitchen, Inc. in the United States 
-      and/or other countries. Other trademarks are the property of their 
-      respective owners. Any use of such trademarks without written permission 
-      sought and obtained from Component Kitchen, Inc. is prohibited.
+      © 2016-2017 Component Kitchen Inc. and contributors to the Elix project.
+      ELIX and the Elix project logo are registered or unregistered trademarks
+      of Component Kitchen Inc. in the United States and/or other countries.
+      Other trademarks are the property of their respective owners. Any use of
+      such trademarks without written permission sought and obtained from
+      Component Kitchen is prohibited.
     </p>
     <p>
-      The Elix Project is administered, as a courtesy, by Component Kitchen, 
-      Inc. for the benefit of the project’s contributors and the developer 
-      community as a whole.
+      The Elix project is administered, as a courtesy, by Component Kitchen for
+      the benefit of the project’s contributors and the developer community as a
+      whole.
     </p>
   </footer>
 );
