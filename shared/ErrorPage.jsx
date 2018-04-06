1a79a5625c1c3b1ac4b1a1f912de5d6fdc164e92
Jan Miksovsky <jan@quickui.org>
2018-04-06 08:58:45 -0700

    ErrorPage passes necessary request param to PageTemplate.

--- shared/ErrorPage.jsx
+++ shared/ErrorPage.jsx
@@ -10,9 +10,11 @@ export default class ErrorPage extends PageTemplate {
   render(props) {
     return (
       <PageTemplate
+          request={props.request}
           title={this.title}
           url={props.url}
         >
+        <h1>{this.title}</h1>
         <p>
           Sorry, something went wrong. 😞
         </p>
